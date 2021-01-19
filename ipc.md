fb8f50f5570c33ba7d92d8a2929e0766fe39eb17
John Mastroberti <johnmastroberti123@gmail.com>
2021-01-19 13:00:42 -0500

    implemented some commands

--- ipc.md
+++ ipc.md
@@ -7,25 +7,48 @@
 
 # Commands to implement
 ## Querry commands
-* how many monitors and what their numbers are
-* which tags are being viewed for a given monitor
-* title of the selected window
-* which layout is active
+* ! how many monitors and what their numbers are
+* ! which tags are being viewed for a given monitor
+* ! title of the selected window
+* ! which layout is active
 ## Instruction commands
-* focusstack (change which client is focused)
-* setmfact
-* killclient (close selected client)
-* setlayout
-* togglefloating
-* focusmon (change which monitor is focused)
-* tagmon (send a client to a given monitor)
+* ! focusstack (change which client is focused)
+* ! setmfact
+* ! killclient (close selected client)
+* ! setlayout
+* ! togglefloating
+* ! focusmon (change which monitor is focused)
+* ! tagmon (send a client to a given monitor)
 * tag commands
-  * view (view a tag)
-  * toggleview (add a tag to be viewed)
-  * tag (send client to a given tag)
-  * toggletag (add a tag to a client)
+  * ! view (view a tag)
+  * ! toggleview (add a tag to be viewed)
+  * ! tag (send client to a given tag)
+  * ! toggletag (add a tag to a client)
 * add/remove rules
-* set borderpx
-* set gappx
-* set reserved space for bar(s)
-* add/remove subscriber pids
+* ! set borderpx
+* ! set gappx
+* ! set reserved space for bar(s)
+* ! add/remove subscriber pids
+
+# Commands
+* mons (prev|next)
+* title (mon)
+* subscribe (pid|reset)
+* set
+  * mfact (mon)
+  * borderpx
+  * gappx
+  * reserved (mon) (left|right|top|bottom)
+  * layout (mon) (layout)
+* tags
+  * list (mon)
+  * view (tag)
+  * toggleview (tag)
+  * set (tag)
+  * toggletag (tag)
+  * mon (prev|next)
+* focus
+  * (prev|next|monprev|monnext)
+* killclient
+* togglefloating
+
--- src/cmd.c
+++ src/cmd.c
@@ -1,12 +1,20 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "cmd.h"
 #include "ipc.h"
 
 void mon_cmd(int connfd, char *msg, Monitor *mons);
+void title_cmd(int connfd, char *msg, Monitor *mons);
+void subscribe_cmd(int connfd, char *msg, Monitor *mons);
+void set_cmd(int connfd, char *msg, Monitor *mons);
 void tag_cmd(int connfd, char *msg, Monitor *mons);
-void unknown_cmd(int connfd, char *cmd);
+void focus_cmd(int connfd, char *msg, Monitor *mons);
+void kill_cmd(int connfd, Monitor *mons);
+void togglefloat_cmd(int connfd, Monitor *mons);
+void unknown_cmd(int connfd);
+
 
 void handle_connection(int connfd, Monitor *mons) {
   char *msg = receive_message(connfd);
@@ -19,29 +27,99 @@ void handle_connection(int connfd, Monitor *mons) {
 
   if (!strcmp(cmd, "mons"))
     mon_cmd(connfd, msg, mons);
+  else if (!strcmp(cmd, "title"))
+    title_cmd(connfd, msg, mons);
   else if (!strcmp(cmd, "tags"))
     tag_cmd(connfd, msg, mons);
   else
-    unknown_cmd(connfd, cmd);
+    unknown_cmd(connfd);
 
   close(connfd);
 }
 
 
+char* mons_list(Monitor *mons) {
+  Monitor *m;
+  char *buf = malloc(1024); // buffer overflow possible
+  int pos = 0;
+  for (m = mons; m; m = m->next)
+    pos += sprintf(buf + pos, "%d ", m->num);
+
+  return buf;
+}
+
 
 void mon_cmd(int connfd, char *msg, Monitor *mons) {
+  Arg arg;
+  char *sub = strtok(msg, " ");
+  if (!strcmp(sub, "prev")) {
+    arg.i = -1;
+    focusmon(&arg);
+    send_message(connfd, "");
+  } else if (!strcmp(sub, "next")) {
+    arg.i = +1;
+    focusmon(&arg);
+    send_message(connfd, "");
+  } else {
+    char *rsp = mons_list(mons);
+    send_message(connfd, rsp);
+    free(rsp);
+  }
+}
+
+
+void title_cmd(int connfd, char *msg, Monitor *mons) {
+  char *sub = strtok(msg, " ");
+  if (!sub) goto title_err;
+  int num = atoi(sub);
+  for (Monitor* m = mons; m; m = m->next) {
+    if (num == m->num) {
+      send_message(connfd, m->sel->name);
+      return;
+    }
+  }
+title_err:
+  send_message(connfd, "ERROR: bad monitor number");
+}
+
+
+void subscribe_cmd(int connfd, char *msg, Monitor *mons) {
   (void) msg;
   (void) mons;
-  send_message(connfd, "mons command called");
+  send_message(connfd, "WARNING: not implemented"); //TODO
 }
+
+
+void set_mfact_cmd(int connfd, char *msg, Monitor *mons);
+void set_borderpx_cmd(int connfd, char *msg);
+void set_gappx_cmd(int connfd, char *msg);
+void set_reserved_cmd(int connfd, char *msg, Monitor *mons);
+void set_layout_cmd(int connfd, char *msg, Monitor *mons);
+
+void set_cmd(int connfd, char *msg, Monitor *mons) {
+  char *sub = strtok(msg, " ");
+  if (!strcmp(sub, "mfact"))
+    set_mfact_cmd(connfd, msg, mons);
+  else if (!strcmp(sub, "borderpx"))
+    set_borderpx_cmd(connfd, msg);
+  else if (!strcmp(sub, "gappx"))
+    set_gappx_cmd(connfd, msg);
+  else if (!strcmp(sub, "reserved"))
+    set_reserved_cmd(connfd, msg, mons);
+  else if (!strcmp(sub, "layout"))
+    set_layout_cmd(connfd, msg, mons);
+  else
+    send_message("ERROR: bad sub command");
+}
+
+
+
 void tag_cmd(int connfd, char *msg, Monitor *mons) {
   (void) msg;
   (void) mons;
   send_message(connfd, "tags command called");
 }
 
-void unknown_cmd(int connfd, char *cmd) {
-  char buf[1024];
-  snprintf(buf, 1024, "unknown command called: %s", cmd);
-  send_message(connfd, buf);
+void unknown_cmd(int connfd) {
+  send_message(connfd, "ERROR: unknown command");
 }
