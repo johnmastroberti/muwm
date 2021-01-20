12808fb7fcde0023865d04b980ffe8911e7d53a1
John Mastroberti <johnmastroberti123@gmail.com>
2021-01-20 15:38:31 -0500

    cleanup

--- include/geom.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct Geom {
-  int x, y, w, h;
-
-  bool operator==(const Geom&) = default;
-}
--- src/client.cpp
+++
@@ -1,6 +0,0 @@
-#include "client.hpp"
-
-// Set up what we can here, rely on XorgConnection to
-// fill in the rest
-Client::Client(Window w, XWindowAttributes* wa) : win{w} {
-
